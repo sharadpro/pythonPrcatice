f86804dbe7a05c1b3ff98486e104b5d71aaa9334
sharad <sharad_singh@trisysit.com>
2021-09-01 00:15:45 +0530

    python dump

--- forloop.py
+++ forloop.py
@@ -1,3 +1,11 @@
 text="python"
 for charector in text:
     print(charector)
+
+languages=["sasd", "asdfsadf", "asdaasd"]
+for charector in languages:
+    print(charector)
+
+
+for count in range(1,6):
+   print( count)
---
+++ sumofnumber.py
@@ -0,0 +1,6 @@
+num=int(input("Enter number to calculate the sum "))
+#num=3
+s1=0
+for n in range(1,(num+1)):
+    s1=s1+n
+print("the sum of ",num," number is ",s1)
